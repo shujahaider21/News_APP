bc4193445f5b1ad37d958f648671a2e7801a8baa
shujahaider21 <shujahaider300@gmail.com>
2025-05-29 22:50:43 +0500

    Implement HeadlineScreen with news fetching and loading state management

--- lib/controllers/news_controller.dart
+++ lib/controllers/news_controller.dart
@@ -7,21 +7,35 @@ import 'package:news_app/models/news_model.dart';
 
 class NewsController extends GetxController {
   var news = <Article>[].obs;
+  var isLoading = false.obs;
   Future<void> getNews() async {
+    isLoading.value = true;
     try {
       var uri = Uri.parse(
         "https://newsapi.org/v2/top-headlines?country=us&apiKey=$apiKey",
       );
       final response = await http.get(uri);
       if (response.statusCode == 200) {
+        debugPrint("News fetched successfully");
+        debugPrint("Response: ${response.body}");
         final jsonResponse = jsonDecode(response.body);
         final newsResponse = NewsResponse.fromJson(jsonResponse);
         news.value = newsResponse.articles;
+        isLoading.value = false;
       } else {
+        isLoading.value = false;
         debugPrint("Failed to load news: ${response.statusCode}");
       }
     } catch (e) {
+      isLoading.value = true;
+
       debugPrint(e.toString());
     }
+  }
+
+  @override
+  void onInit() {
+    getNews();
+    super.onInit();
   }
 }
---
+++ lib/screens/headline_screen.dart
@@ -0,0 +1,81 @@
+import 'package:flutter/material.dart';
+import 'package:get/get.dart';
+import 'package:news_app/controllers/news_controller.dart';
+
+class HeadlineScreen extends StatelessWidget {
+  const HeadlineScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    var contoller = Get.put(NewsController());
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Headlines"),
+        actions: [
+          IconButton(
+            icon: const Icon(Icons.refresh),
+            onPressed: () {
+              contoller.getNews();
+            },
+          ),
+        ],
+      ),
+      body: SafeArea(
+        child: Obx(
+          () => ListView.builder(
+            itemBuilder: (context, index) {
+              if (contoller.isLoading.value) {
+                return Center(
+                  child: CircularProgressIndicator(color: Colors.black),
+                );
+              } else if (contoller.news.isEmpty) {
+                return const Center(child: Text("No News Found!"));
+              } else {
+                return Container(
+                  margin: const EdgeInsets.all(10),
+                  height: 250,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(10),
+                    color: Colors.black.withValues(alpha: 0.4),
+                  ),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ClipRRect(
+                          borderRadius: BorderRadius.circular(10),
+                          child: Image.network(
+                            contoller.news[index].urlToImage ?? '',
+                            fit: BoxFit.cover,
+                            height: 150,
+                            width: double.infinity,
+                          ),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              contoller.news[index].title.length > 20
+                                  ? "${contoller.news[index].title.substring(0, 20)}..."
+                                  : contoller.news[index].title,
+                            ),
+                            Text(contoller.news[index].publishedAt),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }
+            },
+            itemCount: contoller.news.length,
+          ),
+        ),
+      ),
+    );
+  }
+}
